3fa972db171171bf91d00a8aacbf6089b78eb83e
haomingjue <haomingjue@outlook.com>
2022-08-02 23:23:41 -0400

    setup backedn repo

---
+++ spring-boot-product-store/src/main/java/com/sha/springbootproductstore/SpringBootProductStoreApplication.java
@@ -0,0 +1,13 @@
+package com.sha.springbootproductstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProductStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProductStoreApplication.class, args);
+	}
+
+}
---
+++ spring-boot-product-store/src/test/java/com/sha/springbootproductstore/SpringBootProductStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sha.springbootproductstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProductStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
